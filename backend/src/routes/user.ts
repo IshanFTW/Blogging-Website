81e16d455ca1a5e2e0624b6e8b87dff65ec8a4a9
IshanFTW <ishanjindal2003@gmail.com>
2024-03-31 21:20:54 +0530

    zod validation

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 import { sign } from 'hono/jwt'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { signupInput } from "@ishhhan/medium-common";
+import { signipInput } from "@ishhhan/medium-common";
 
 export const userRouter = new Hono<{
     Bindings: {
@@ -15,8 +17,16 @@ userRouter.post('/signup', async (c) =>{
     datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
   
-    const body = await c.req.json();
+    
     try{
+      const body = await c.req.json();
+      const { success } = signupInput.safeParse(body);
+      if (!success){
+        c.status(411)
+        return c.json({
+          message: "Incorrect input"
+        })
+      }
       const user = await prisma.user.create({
         data: {
           email: body.email,
@@ -39,6 +49,13 @@ userRouter.post('/signup', async (c) =>{
     
     try{
       const body = await c.req.json();
+      const { success } = signipInput.safeParse(body);
+      if (!success){
+        c.status(411)
+        return c.json({
+          message: "Incorrect input"
+        })
+      }
       const user = await prisma.user.findUnique({
         where: {
           email: body.email,
